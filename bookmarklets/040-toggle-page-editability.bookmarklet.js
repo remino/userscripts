c9c7aaabb9f073b8c557dfc70735a8526a33c875
Rémino Rem <29999+remino@users.noreply.github.com>
2023-11-27 09:28:39 +0900

    Add page editability & blended links bookmarklets

---
+++ bookmarklets/040-toggle-page-editability.bookmarklet.js
@@ -0,0 +1,6 @@
+// @title Toggle Page Editability
+(function togglePageEditability() {
+	'use strict'
+
+	document.body.contentEditable = document.body.contentEditable === 'true' ? 'false' : 'true'
+}())
